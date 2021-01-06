23d458cf481db2d3f7321a4f90d52f3c139d16d1
Mark <mark@bantam-tech.com>
2021-01-06 11:13:52 -0500

    Changed simple-buffer-test to new format (tests need rewriting)

--- test/simple-buffer-test.c
+++ test/simple-buffer-test.c
@@ -4,6 +4,10 @@
 
 #include "machine.h"
 #include "simple-buffers.h"
+#include "test.h"
+
+#define LOGEX_TAG "BUF-TEST"
+#include <logex-main.h>
 
 static void
 print_buf(char *buf, size_t bytes) {
@@ -21,36 +25,71 @@ print_buf(char *buf, size_t bytes) {
     printf("\n");
 }
 
+static int
+rb_write()
+{
+    return 1;
+}
+
+static int
+rb_min_return_size()
+{
+    return 1;
+}
+
 int
-main(int argc, char *argv[]) {
-    IO_HANDLE h;
-    const IOM *fbb = new_fbb_machine(&h, 2, 10);
+fbb()
+{
+    int ret = 1;
+
+    IO_HANDLE h = new_fbb_machine(2, 10);
 
     char buf[20];
     size_t bytes;
 
     memset(buf, 'A', 20);
     bytes = 20;
-    fbb->write(h, buf, &bytes);
+    fbb_machine->write(h, buf, &bytes);
+
+    memset(buf, 'A', 20);
     bytes = 10;
-    fbb->write(h, buf, &bytes);
+    fbb_machine->write(h, buf, &bytes);
 
     memset(buf, 'B', 20);
     bytes = 5;
-    fbb->write(h, buf, &bytes);
+    fbb_machine->write(h, buf, &bytes);
 
     memset(buf, 'C', 20);
     bytes = 1;
-    fbb->write(h, buf, &bytes);
+    fbb_machine->write(h, buf, &bytes);
 
     bytes = 1;
-    fbb->read(h, buf, &bytes);
+    fbb_machine->read(h, buf, &bytes);
     print_buf(buf, bytes);
 
     int i = 0;
     for (; i < 4; i++) {
         bytes = 20;
-        fbb->read(h, buf, &bytes);
+        fbb_machine->read(h, buf, &bytes);
         print_buf(buf, bytes);
     }
+
+do_return:
+    fbb_machine->destroy(h);
+    return ret;
+}
+
+int
+main(int nargs, char *argv[])
+{
+    test_init_logging(); 
+
+    test_setup();
+
+    test_add(rb_write);
+    test_add(rb_min_return_size);
+    test_add(fbb);
+
+    test_run();
+    test_cleanup();
 }
