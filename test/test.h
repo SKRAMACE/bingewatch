1342e5ecdfcb14aa82f9b45eba05a9cfc142312d
Mark <mark@bantam-tech.com>
2021-01-06 10:56:21 -0500

    Added a universal logging init #define to test infrastructure

--- test/test.h
+++ test/test.h
@@ -1,9 +1,17 @@
 #ifndef __BW_TEST_H__
 #define __BW_TEST_H__
 
+#include <envex.h>
+
 typedef int (*test_fn)(void);
 
 extern const char *bw_test_rootdir;
+
+#define test_init_logging() {\
+    char *__bw_test_log_level; \
+    ENVEX_TOSTR_UPPER(__bw_test_log_level, "BW_UNIT_TEST_LOG_LEVEL", "error"); \
+    set_log_level_str(__bw_test_log_level); \
+}
 
 #define test_add(x) _test_add(x, #x)
 int _test_add(test_fn test, const char *name);
