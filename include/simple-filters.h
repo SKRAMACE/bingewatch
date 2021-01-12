7c852e99995c1a2dd691a0b1d01a950f07c960d7
Mark <mark@bantam-tech.com>
2021-01-12 16:35:52 -0500

    Added time limit filter

--- include/simple-filters.h
+++ include/simple-filters.h
@@ -19,6 +19,7 @@ int byte_counter(IO_FILTER_ARGS);
 int byte_count_limiter(IO_FILTER_ARGS);
 
 struct io_filter_t *create_byte_count_limit_filter(void *alloc, const char *name, size_t byte_limit);
+struct io_filter_t *create_time_limit_filter(void *alloc, const char *name, size_t time_ms);
 struct io_filter_t *create_byte_counter_filter(void *alloc, const char *name, size_t bytes_per_sample);
 struct io_filter_t *create_conversion_filter(void *alloc, const char *name, int from_fmt, int to_fmt, int data_precision);
 
--- src/filter/filters.c
+++ src/filter/filters.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #include "machine.h"
 #include "segment.h"
 #include "filter.h"
+
+enum time_limit_state_e {
+    TIME_LIMIT_NOINIT=0,
+    TIME_LIMIT_RUNNING,
+    TIME_LIMIT_DONE,
+};
+    
+struct time_limit_t {
+    enum time_limit_state_e state;
+    pthread_t thread;
+    size_t ms;
+};
 
 struct generic_counter_t {
     size_t total;
@@ -58,6 +72,54 @@ create_byte_count_limit_filter(void *alloc, const char *name, size_t byte_limit)
     limit->total = 0;
     limit->limit = byte_limit;
     f->obj = limit;
+    return f;
+}
+
+static void *
+run_timer(void *args)
+{
+    struct time_limit_t *time = (struct time_limit_t *)args;
+
+    size_t ms = 0;
+    while (ms++ < time->ms) {
+        usleep(1000);
+    }
+    time->state = TIME_LIMIT_DONE;
+}
+
+static int
+time_limiter(IO_FILTER_ARGS)
+{
+    IOF_DISABLED();
+
+    struct time_limit_t *time = (struct time_limit_t *)IO_FILTER_ARGS_FILTER->obj;
+    switch (time->state) {
+    case TIME_LIMIT_NOINIT:
+        time->state = TIME_LIMIT_RUNNING;
+        pthread_create(&time->thread, NULL, run_timer, (void *)time);
+        break;
+
+    case TIME_LIMIT_RUNNING:
+        break;
+
+    case TIME_LIMIT_DONE:
+        pthread_join(time->thread, NULL);
+        return IO_COMPLETE;
+
+    default:
+        return IO_ERROR;
+    }
+        
+    return CALL_NEXT_FILTER();
+}
+
+struct io_filter_t *
+create_time_limit_filter(void *alloc, const char *name, size_t time_ms)
+{
+    struct io_filter_t *f = create_filter(alloc, name, time_limiter);
+    struct time_limit_t *time = pcalloc(alloc, sizeof(struct time_limit_t));
+    time->ms = time_ms;
+    f->obj = time;
     return f;
 }
 
